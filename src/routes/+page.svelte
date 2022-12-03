ca12ebd1450045a0ac00cc4c86994a8ae3b03000
York <111544765+helloyork@users.noreply.github.com>
2022-12-02 22:21:15 -0800

    test20221202

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { spring } from 'svelte/motion';
+
+	let pos = spring(
+		{ x: 50, y: 50 },
+		{
+			stiffness: 0.1,
+			damping: 0.25
+		}
+	);
+
+	let size = spring(10);
+</script>
+
+<svg
+	on:mousemove={(e) => pos.set({ x: e.clientX, y: e.clientY })}
+	on:mousedown={() => size.set(30)}
+	on:mouseup={() => size.set(10)}
+	style="height: 100%;width:100%"
+>
+	<circle fill="red" cx={$pos.x} cy={$pos.y} r={$size} />
+</svg>
---
+++ src/routes/test.svelte
@@ -0,0 +1,4 @@
+<!-- <script lang="ts">
+    export let text='awa'
+</script> -->
+<slot></slot>
