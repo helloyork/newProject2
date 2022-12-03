3f6a0b18f22515647cb09f9e949e311010deaf2f
York <111544765+helloyork@users.noreply.github.com>
2022-12-02 22:28:27 -0800

    test20221202

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { spring } from 'svelte/motion';
+	import { getContext, setContext } from 'svelte/types/runtime/internal/lifecycle';
 
 	let pos = spring(
 		{ x: 50, y: 50 },
@@ -8,14 +9,13 @@
 			damping: 0.25
 		}
 	);
-
 	let size = spring(10);
 </script>
 
 <svg
 	on:mousemove={(e) => pos.set({ x: e.clientX, y: e.clientY })}
 	on:mousedown={() => size.set(30)}
-	on:mouseup={() => size.set(10)}
+	on:mouseup={() => {size.set(10);}}
 	style="height: 100%;width:100%"
 >
 	<circle fill="red" cx={$pos.x} cy={$pos.y} r={$size} />
